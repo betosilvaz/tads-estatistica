c5afcd637493c743d97f0066a62f4e6033b15ed4
Gilberto Silva <49136324+betosilvaz@users.noreply.github.com>
2025-08-18 23:19:13 +0000

    [build]: implemented web interface with streamlit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from functions.plot_ts import plot
+
+def printTicker(ticker):
+    st.plotly_chart(plot(ticker))
+
+st.title("Histórico de cotações")
+st.write("Veja o histórico das cotações")
+ticker = st.sidebar.text_input("Escolha o ticker: ", value="AAPL")
+st.sidebar.button(label="procurar", on_click=printTicker(ticker))
--- functions/plot_ts.py
+++ functions/plot_ts.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import plotly.express as px
 
 def plot(ticker:str):
-    data = yf.download(ticker, period='max', multi_level_index=False)
+    data = yf.download(ticker, period='1y', multi_level_index=False)
     df = data.reset_index()[['Date', 'Close']]
     figure = px.line(df, x="Date", y="Close", title=f"{ticker} MARKET DATA")
-    figure.show()
+    return figure
