4cc78008eee877bd877553898b61a64b519ef324
Gilberto Silva <49136324+betosilvaz@users.noreply.github.com>
2025-08-25 22:39:55 +0000

    added documentation for plot_ts.py

--- functions/plot_ts.py
+++ functions/plot_ts.py
@@ -2,6 +2,15 @@ import yfinance as yf
 import plotly.express as px
 
 def plot(ticker:str):
+    """
+    Plot a time series
+
+    args:
+        ticker(str): the company ticker.
+
+    return:
+        A plotly ticker chart
+    """
     data = yf.download(ticker, period='1y', multi_level_index=False)
     df = data.reset_index()[['Date', 'Close']]
     figure = px.line(df, x="Date", y="Close", title=f"{ticker} MARKET DATA")
